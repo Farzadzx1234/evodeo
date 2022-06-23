05ba2623990230c64bfb6fda02b577981b369b64
Farzad <87334759+Farzadzx1234@users.noreply.github.com>
2022-06-23 21:52:19 +0430

    Update and rename mhelp.js to help.js

--- commands/help.js
+++ commands/help.js
@@ -2,15 +2,15 @@ const { MessageEmbed } = require("discord.js");
 const i18n = require("../util/i18n");
 
 module.exports = {
-  name: "mhelp",
-  aliases: ["mh"],
+  name: "help",
+  aliases: ["h"],
   description: i18n.__("help.description"),
   execute(message) {
     let commands = message.client.commands.array();
 
     let helpEmbed = new MessageEmbed()
-      .setTitle(i18n.__mf("Syma Help Music Menu", { botname: message.client.user.username }))
-      .setDescription(i18n.__("\n **Syma Bot\nDeveloped By AnimeClub Team\n\nBot Prefix ➜ `+`\n\n🇱🇷〢 Just English\n🇮🇷〢Persian (Coming Soon)\n\nFor Bot Invite ➜ `-invite`\nNeed help? Click `🗣️ Server Support` Button\n\nBest ~ Farzad**\n\n📌 **Help Panel** 📌\n"))
+      .setTitle(i18n.__mf("Music Help panel", { botname: message.client.user.username }))
+      .setDescription(i18n.__("**Really Useful Commands**"))
       .setColor("#ffffff");
     commands.forEach((cmd) => {
       helpEmbed.addField(
