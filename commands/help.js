f5205c5d78e59deedd47bc48259192c3c231f0d2
Farzad <87334759+Farzadzx1234@users.noreply.github.com>
2022-06-25 17:49:08 +0430

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -10,9 +10,9 @@ module.exports = {
 
     let helpEmbed = new MessageEmbed()
       .setTitle(i18n.__mf("Oxygen Music", { botname: message.client.user.username }))
-      .setDescription("\n**Oxygen Music**\n─────────────────────────\n> <:Oxygen24:989619598502543462>\n`Pause the currently playing music`\n\n > <:Oxygen16:989600379094319214>\n`Mute and Unmute the music`\n\n> <:Oxygen25:989622067945173094>\n`Change volume of currently playing music`\n\n> <:Oxygen23:989619562955825152>\n`Toggle music loop`\n\n > <:Oxygen12:989600371926245488> \n `Stop the music` ")
+      .setDescription("\n─────────────────────────\n> <:Oxygen24:989619598502543462>\n`Pause the currently playing music`\n\n > <:Oxygen16:989600379094319214>\n`Mute and Unmute the music`\n\n> <:Oxygen25:989622067945173094>\n`Change volume of currently playing music`\n\n> <:Oxygen23:989619562955825152>\n`Toggle music loop`\n\n > <:Oxygen12:989600371926245488> \n `Stop the music` ")
       .setImage('https://cdn.discordapp.com/attachments/985145928501567538/989924167044440064/music.png')
-      .setColor("#ffffff");
+      .setColor("#a077ff");
 
     return message.channel.send(helpEmbed).catch(console.error);
   }
