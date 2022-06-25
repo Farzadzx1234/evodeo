2650f3bcefc972c60be7be65c710fe2c40bcaf17
Farzad <87334759+Farzadzx1234@users.noreply.github.com>
2022-06-25 17:23:44 +0430

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -2,42 +2,17 @@ const { MessageEmbed } = require("discord.js");
 const i18n = require("../util/i18n");
 
 module.exports = {
-  name: "help",
-  aliases: ["h"],
+  name: "mhelp",
+  aliases: ["mh"],
   description: i18n.__("help.description"),
   execute(message) {
     let commands = message.client.commands.array();
 
     let helpEmbed = new MessageEmbed()
-      .setTitle(i18n.__mf("Music Help panel", { botname: message.client.user.username }))
-      .setDescription(" **Oxygen Music**
-─────────────────────────
-> \<:Oxygen24:989619598502543462>
-`Pause the currently playing music`
-
-> \<:Oxygen16:989600379094319214>
-`Mute and Unmute the music`
-
-> \<:Oxygen25:989622067945173094>
-`Change volume of currently playing music`
-
-> \<:Oxygen23:989619562955825152>
-`Toggle music loop`
-
-> \<:Oxygen12:989600371926245488>
-`Stop the music` ")
-    .setImage('https://cdn.discordapp.com/attachments/985145928501567538/989924167044440064/music.png') 
+      .setTitle(i18n.__mf("Oxygen Music", { botname: message.client.user.username }))
+      .setDescription("\n**Oxygen Music**\n─────────────────────────\n> <:Oxygen24:989619598502543462>\n`Pause the currently playing music`\n\n > <:Oxygen16:989600379094319214>\n`Mute and Unmute the music`\n\n> <:Oxygen25:989622067945173094>\n`Change volume of currently playing music`\n\n> <:Oxygen23:989619562955825152>\n`Toggle music loop`\n\n > <:Oxygen12:989600371926245488> \n `Stop the music` ")
+      .setImage('https://cdn.discordapp.com/attachments/985145928501567538/989924167044440064/music.png')
       .setColor("#ffffff");
-    commands.forEach((cmd) => {
-      helpEmbed.addField(
-        `**${message.client.prefix}${cmd.name} ${cmd.aliases ? `(${cmd.aliases})` : ""}**`,
-        `${cmd.description}`,
-        true
-      );
-    });
-
-    helpEmbed.setTimestamp();
-    helpEmbed.setFooter(`Message ID: ${message.id}`)
 
     return message.channel.send(helpEmbed).catch(console.error);
   }
