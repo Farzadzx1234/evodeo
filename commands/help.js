65608d875ec32a422f3befc6267898eab70b4a20
Farzad <87334759+Farzadzx1234@users.noreply.github.com>
2022-06-25 17:05:05 +0430

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -10,7 +10,22 @@ module.exports = {
 
     let helpEmbed = new MessageEmbed()
       .setTitle(i18n.__mf("Music Help panel", { botname: message.client.user.username }))
-      .setDescription(i18n.__("**Really Useful Commands**"))
+      .setDescription(" **Oxygen Music**
+─────────────────────────
+> \<:Oxygen24:989619598502543462>
+`Pause the currently playing music`
+
+> \<:Oxygen16:989600379094319214>
+`Mute and Unmute the music`
+
+> \<:Oxygen25:989622067945173094>
+`Change volume of currently playing music`
+
+> \<:Oxygen23:989619562955825152>
+`Toggle music loop`
+
+> \<:Oxygen12:989600371926245488>
+`Stop the music` ")
     .setImage('https://cdn.discordapp.com/attachments/985145928501567538/989924167044440064/music.png') 
       .setColor("#ffffff");
     commands.forEach((cmd) => {
