674cb1c2f4c58e7450439ba77b3c629e31a256ea
Farzad <87334759+Farzadzx1234@users.noreply.github.com>
2022-06-27 00:02:29 +0430

    Update play.js

--- include/play.js
+++ include/play.js
@@ -139,7 +139,7 @@ module.exports = {
             
           } else {
             queue.playing = !queue.playing;
-            queue.connection.dispatcher.resume(true);
+            queue.connection.dispatcher.resume();
             
           }
           break;
